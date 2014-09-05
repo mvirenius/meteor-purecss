fb2c14820f527772649144241c904f1c608f6566
Mikko Virenius <mikko.virenius@adian.fi>
2014-09-05 18:17:02 +0300

    Migrate to Meteor 0.9.1

--- package.js
+++ package.js
@@ -1,7 +1,14 @@
 Package.describe({
-    summary: "Pure is a set of small, responsive CSS modules that you can use in every web project."
+    summary: "Pure is a set of small, responsive CSS modules that you can use in every web project.",
+  version: "0.5.0",
+  git: "https://github.com/MikkoVirenius/meteor-purecss.git"
 });
 
-Package.on_use(function (api) {
-    api.add_files(['css/pure-min.css'], 'client');
+Package.onUse(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
+
+  var path = Npm.require('path');
+  var asset_path = path.join('css');
+  api.addFiles(path.join(asset_path, 'pure-min.css'), 'client');
+
 });
